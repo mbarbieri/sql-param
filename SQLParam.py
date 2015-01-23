24744da83cd36617676b16b7a5943612e90818af
Matteo Barbieri <matteo.barbieri@gruppopam.it>
2015-01-23 15:26:40 +0100

    Add date parsing and new lines removal

--- SQLParam.py
+++ SQLParam.py
@@ -2,11 +2,15 @@ import re
 
 import sublime
 import sublime_plugin
+from datetime import datetime
 
 class SqlParamCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         view = self.view
         text = view.substr(sublime.Region(0, view.size()))
+
+        query_region = sublime.Region(0, view.find("-----",0).begin())
+        self.remove_newlines(edit, query_region)
         params_strings = view.find_all("^PARAM", sublime.IGNORECASE)
 
         params = {}
@@ -14,16 +18,25 @@ class SqlParamCommand(sublime_plugin.TextCommand):
         for param in params_strings:
             line = view.substr(view.full_line(param))
             m =re.match(r"^PARAM (\d+) (.*)", line)
-            params[int(m.group(1))] = m.group(2)
-
-        print(view.find_all("\\?"))
+            try:
+                date = datetime.strptime(m.group(2)[0:19], '%Y-%m-%d %H:%M:%S')
+                params[int(m.group(1))] = "'" + date.strftime('%Y-%m-%d') + "'"
+            except ValueError as e:
+                params[int(m.group(1))] = m.group(2)
 
         i = 0
         while view.find("\\?", 0):
             region = view.find("\\?", 0)
-            print(str(i)+": replace "+view.substr(region) + " with "+params[i])
             view.replace(edit, region, params[i])
             i += 1
 
         view.run_command('sql_beautifier')
+        self.comment_last_line(edit)
 
+    def remove_newlines(self, edit, query_region):
+        query_string_cleaned = self.view.substr(query_region).replace('\n','')
+        self.view.replace(edit, query_region, query_string_cleaned)
+
+    def comment_last_line(self, edit):
+        last_line = self.view.line(self.view.size())
+        self.view.replace(edit, last_line, '-- '+self.view.substr(last_line))
